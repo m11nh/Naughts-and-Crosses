621588d16f84f8b4e6dfd0236fc3a68ae5f92654
Minh <z5127036@ad.unsw.edu.au>
2020-10-14 20:10:01 +1100

    basic test file skeleton

---
+++ testNAC.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "NAC.h"
+
+static void testNewGame(); 
+static void testEnterMove(); 
+static void testGetMove(); 
+static void testValidMove(); 
+static void testPlayersTurn(); 
+static void testGameIsOver(); 
+static void testPlayerHasWon(); 
+
+int main() {testNewGame(); 
+    testEnterMove(); 
+    testGetMove(); 
+    testValidMove(); 
+    testPlayersTurn(); 
+    testGameIsOver(); 
+    testPlayerHasWon(); 
+}
+
+static void testNewGame() {} 
+static void testEnterMove() {} 
+static void testGetMove() {} 
+static void testValidMove() {} 
+static void testPlayersTurn() {} 
+static void testGameIsOver() {} 
+static void testPlayerHasWon() {} 
