29abd5cbdb1bca7620287100a6c011810da3b4e1
Minh <minh.nguyen19997@hotmail.com>
2020-10-16 20:59:26 +1100

    completed function to display a board

---
+++ NACview.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "NACview.h"
+
+void displayBoard(Game g) {
+    for (int i = 0; i < tableSize; i++) {
+        for (int j = 0; j < tableSize; j++) {
+            int move = getMove(g, j, i); 
+            if (move == Naught) {
+                printf(" X "); 
+            } else if (move == Cross) {
+                printf(" O "); 
+            } else {
+                printf("   "); 
+            }
+        }
+        printf("\n"); 
+    }
+}
+
