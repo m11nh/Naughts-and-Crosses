277c1fb30c41995e7b7238759b113ba3a5b40af7
Minh <minh.nguyen19997@hotmail.com>
2020-10-17 18:32:53 +1100

    removed planning comments

--- NAC.h
+++ NAC.h
@@ -20,20 +20,3 @@ int getEmptySlots(Game g);
 bool playerHasWon(Player p, Game g); 
 
 #endif
-/*
-Structs that i wil need 
-
-gameState
-- playerTurn - either 0, 1, for players
-- game rep - array[3][3] - 0, and 1 representing whether naught or cross on a certain location, -1 if currently empty
-- emptySlots - to check if there still are turns needed
-
-Brainstorming functions I may need
-
-playerHasWon(player, gameState)
-playerHasLost(player, gameState)
-gameIsOver(gameState)
-enterMove(gameState, move)
-isPlayersTurn(player, gameState) 
-
-*/ 
