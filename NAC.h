1fc7d41209a37d58dbef12c22c06a6410fc66977
Minh <minh.nguyen19997@hotmail.com>
2020-11-06 21:24:06 +1100

    added comments to .h functions

--- NAC.h
+++ NAC.h
@@ -13,15 +13,60 @@ typedef struct gameState *Game;
 typedef enum { Naught, Cross, None =-1 } Player; 
 
 bool player(int player, Game g);
+
+/*
+    Returns True if game is over, otherwise false. 
+*/ 
 bool gameIsOver(Game g); 
+
+/*
+    Takes in a move, and if move is valid in current game state, the game state is updated 
+    and the move is entered. 
+*/
 void enterMove(Game g, Move m); 
+
+/*
+    Returns true if move is valid in current game state, otherwise returns false. 
+*/
 bool validMove(Game g, Move m);
+
+/*
+    Creates a new game, with p player being the player with the starting move. 
+*/
 Game newGame(Player p); 
+
+/*
+    Removes all game related data specified by given game instance. 
+*/
 void endGame(Game g); 
+
+/*
+    Returns the player who has the current turn.
+*/
 Player getPlayerTurn(Game g); 
+
+/*
+    Returns the player who made a move at position given. 
+    If no move has been made at the position then -1 is returned. 
+*/
 Player getCoordState(Game g, int x, int y); 
+
+/*
+    Returns an integer, representing the number of slots on the board that
+    has not been occupied by a player move yet. 
+*/
 int getEmptySlots(Game g); 
+
+/*
+    Returns the player who has won the game. If the game has not been 
+    won by any player, returns -1. 
+*/ 
 Player getWinner(Game g); 
+
+/*
+    Returns true if game has ended, and game is a draw
+    otherwise returns false
+*/ 
 bool gameIsDraw(Game g); 
 
 #endif
--- NACview.h
+++ NACview.h
@@ -6,10 +6,35 @@
 #ifndef NACVIEW_h
 #define NACVIEW_H 
 
+/*
+    Provides a visual display of the game state.
+    If isVerbose is set to true, the axis lines are displayed, otherwise
+    the axis lines are not displayed. 
+*/
 void displayBoard(Game g, bool isVerbose); 
+
+/*
+    Asks player p for their move, and stores it into the provided move pointer. 
+*/
 bool getMove(Player p, Move *m); 
+
+/*
+    returns a string representation of the name of player. 
+    i.e. Naught = "naught"
+         Cross = "cross"
+*/
 void getPlayerString(Player p, char *str); 
+
+/*
+    Displays an error message saying the the move was not valid. 
+*/
 void showInvalidMoveError(); 
+
+/*
+    Displays the state of the game after it has ended. 
+    If there is a winner, the name of the winner is declared. 
+    If there is a draw, the user is notified that the game is a draw. 
+*/
 void showEndGameMsg(Game g);
 
 #endif
