fbf872cb2a59d5352e7b1088379c3619824919e8
Minh <minh.nguyen19997@hotmail.com>
2020-11-25 20:40:54 +1100

    completed testing

--- testNAC.c
+++ testNAC.c
@@ -3,23 +3,80 @@
 
 #include "NAC.h"
 
-static void testNewGame(); 
-static void testEnterMove(); 
-static void testGetMove(); 
-static void testValidMove(); 
-static void testPlayersTurn(); 
 static void testGameIsOver(); 
-static void testPlayerHasWon(); 
+static void testEnterMove(); 
+static void testValidMove(); 
+static void testNewGame(); 
+static void testGetPlayerTurn(); 
+static void testGetCoordState(); 
+static void testGetEmptySlots(); 
+static void testGetWinner(); 
+static void testGameIsDraw(); 
 
 int main() {
-    testNewGame(); 
-    testEnterMove(); 
-    testGetMove(); 
-    testValidMove(); 
-    testPlayersTurn(); 
     testGameIsOver(); 
-    testPlayerHasWon(); 
+    testEnterMove(); 
+    testValidMove(); 
+    testNewGame(); 
+    testGetPlayerTurn(); 
+    testGetCoordState(); 
+    testGetEmptySlots(); 
+    testGetWinner(); 
+    testGameIsDraw(); 
 }
+
+static void testGameIsOver() {
+    printf("Testing gameIsOver...\n"); 
+    Player c = Cross; 
+    Game g = newGame(c); 
+    Move m; 
+    bool stop = false; 
+    assert(gameIsOver(g) == false); 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if ((i == 2) && (j == 1)) {
+                stop = true; 
+                break; 
+            }
+            assert(gameIsOver(g) == false); 
+            m.x = i; 
+            m.y = j; 
+            enterMove(g, m); 
+        }
+        if (stop) {
+            break; 
+        }
+    }
+    assert(gameIsOver(g) == true); 
+    printf("gameIsOver tests passed!\n"); 
+} 
+
+static void testEnterMove() {
+    printf("Testing enterMove...\n"); 
+    Player c = Cross; 
+    Game g = newGame(c); 
+    Move m; 
+    m.x = 0; 
+    m.y = 0; 
+    enterMove(g, m);
+    assert(getCoordState(g, 0, 0) == c);
+    printf("enterMove tests passed!\n"); 
+} 
+
+static void testValidMove() {
+    printf("Testing validMove...\n"); 
+    Player c = Cross; 
+    Game g = newGame(c); 
+    Move m; 
+    m.x = 0; 
+    m.y = 0; 
+    assert(validMove(g, m) == true); 
+    m.x = 1; 
+    m.y = 2; 
+    enterMove(g, m); 
+    assert(validMove(g, m) == false); 
+    printf("validMove tests passed!\n"); 
+} 
 
 static void testNewGame() {
     printf("Testing newGame...\n"); 
@@ -30,149 +87,121 @@ static void testNewGame() {
     printf("newGame tests passed!\n"); 
 } 
 
-static void testEnterMove() {
-    printf("Testing enterMove...\n"); 
+static void testGetPlayerTurn() {
+    printf("Testing playerTurn...\n"); 
     Player n = Naught; 
     Player c = Cross; 
     Game g = newGame(c); 
-    enterMove(g, c, 1, 1); 
-    assert(getMove(g, 1, 1) == c);
-    enterMove(g, n, 1, 2); 
-    assert(getMove(g, 1, 2) == n); 
-    assert(getMove(g, 0, 0) == -1); 
-    printf("enterMove tests passed!\n"); 
-} 
-
-static void testGetMove() {
-    printf("Testing getMove...\n"); 
-    Player n = Naught; 
-    Player c = Cross; 
-    Game g = newGame(c); 
-    enterMove(g, c, 1, 1); 
-    assert(getMove(g, 1, 1) == c);
-    enterMove(g, n, 1, 2); 
-    assert(getMove(g, 1, 2) == n); 
-    assert(getMove(g, 0, 0) == -1); 
-    printf("getMove tests passed!\n"); 
-} 
-
-static void testValidMove() {
-    printf("Testing validMove...\n"); 
-    Player c = Cross; 
-    Game g = newGame(c); 
-    assert(validMove(g, 0, 0) == true); 
-    enterMove(g, c, 1, 2); 
-    assert(validMove(g, 1, 2) == false); 
-    printf("validMove tests passed!\n"); 
-} 
-
-static void testPlayersTurn() {
-    printf("Testing playersTurn...\n"); 
-    Player n = Naught; 
-    Player c = Cross; 
-    Game g = newGame(c); 
+    Move m; 
     assert(getPlayerTurn(g) == c);
-    enterMove(g, c, 1, 2); 
+    m.x = 1; 
+    m.y = 2; 
+    enterMove(g, m); 
     assert(getPlayerTurn(g) == n);
-    enterMove(g, n, 1, 2); 
+    m.x = 2;
+    m.y = 2;
+    enterMove(g, m); 
     assert(getPlayerTurn(g) == c);
-    printf("playersTurn tests passed!\n"); 
+    printf("getPlayerTurn tests passed!\n"); 
 } 
 
-static void testGameIsOver() {
-    printf("Testing gameIsOver...\n"); 
+static void testGetCoordState() {
+    printf("Testing getCoordState...\n"); 
     Player c = Cross; 
-    Player n = Naught; 
-    Player turn = c; 
     Game g = newGame(c); 
+    Move m; 
+    m.x = 0; 
+    m.y = 0; 
+    assert(getCoordState(g, 0, 0) == -1);
+    assert(getCoordState(g, 0, 1) == -1);
+    enterMove(g, m);
+    assert(getCoordState(g, 0, 0) == c);
+    assert(getCoordState(g, 0, 1) == -1);
+    printf("getCoordState tests passed!\n"); 
+}
+
+static void testGetEmptySlots() {
+    printf("Testing getEmptySlots...\n"); 
+    // empty slots when game initialised - should be number of squares
+    Player c = Cross; 
+    Game g = newGame(c); 
+    Move m; 
+    assert(getEmptySlots(g) == tableSize * tableSize);
+    // empty slots when a move has made - should decrement by 1
+    m.x = 0; 
+    m.y = 0; 
+    enterMove(g, m); 
+    assert(getEmptySlots(g) == tableSize * tableSize - 1);
+    printf("getEmptySlots tests passed!\n"); 
+}
+
+static void testGetWinner() {
+    printf("Testing getWinner...\n"); 
+    Player c = Cross; 
+    Game g = newGame(c); 
+    Move m; 
+    bool stop = false; 
     assert(gameIsOver(g) == false); 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            assert(gameIsOver(g) == false); 
-            enterMove(g, turn, i, j); 
-            turn = (turn == c) ? n : c; 
+            if ((i == 2) && (j == 1)) {
+                stop = true; 
+                break; 
+            }
+            assert(getWinner(g) == -1); 
+            m.x = i; 
+            m.y = j; 
+            enterMove(g, m); 
+        }
+        if (stop) {
+            break; 
         }
     }
-    assert(gameIsOver(g) == true); 
-    printf("gameIsOver tests passed!\n"); 
-} 
+    assert((getWinner(g) == Cross) || (getWinner(g) == Naught)); 
+    printf("getWinner tests passed!\n"); 
+}
 
-static void testPlayerHasWon() {
-    printf("Testing playerHasWon...\n"); 
-    Player n = Naught; 
-    Player c = Cross; 
-    Game g = newGame(n); 
-
-    // vert case
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, n, 0, 0); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 1, 0); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, n, 0, 1); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 1, 1); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, n, 0, 2); 
-    assert(playerHasWon(n, g) == true); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 2, 0); 
-    assert(playerHasWon(n, g) == true); 
-    assert(playerHasWon(c, g) == false); 
-
-    endGame(g); 
-    g = newGame(n); 
-
-    // horiz case
-    enterMove(g, n, 0, 2); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 0, 1); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, n, 1, 2); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 1, 1); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, n, 2, 2); 
-    assert(playerHasWon(n, g) == true); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 1, 0); 
-    assert(playerHasWon(n, g) == true); 
-    assert(playerHasWon(c, g) == false); 
-
-    endGame(g); 
-    g = newGame(n); 
-
-    // diagonal case
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, n, 0, 0); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 0, 1); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, n, 1, 1); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 1, 0); 
-    assert(playerHasWon(n, g) == false); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, n, 2, 2); 
-    assert(playerHasWon(n, g) == true); 
-    assert(playerHasWon(c, g) == false); 
-    enterMove(g, c, 0, 2); 
-    assert(playerHasWon(n, g) == true); 
-    assert(playerHasWon(c, g) == false); 
-
-    endGame(g); 
-    printf("playerHasWon tests passed!\n"); 
-} 
+static void testGameIsDraw() {
+    printf("Testing gameIsDraw...\n"); 
+    Player c = Naught; 
+    Game g = newGame(c); 
+    Move m; 
+    assert(gameIsDraw(g) == false);
+    m.x = 2;
+    m.y = 0;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == false);
+    m.x = 0;
+    m.y = 0;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == false);
+    m.x = 0;
+    m.y = 1;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == false);
+    m.x = 0;
+    m.y = 2;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == false);
+    m.x = 1;
+    m.y = 1;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == false);
+    m.x = 1;
+    m.y = 0;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == false);
+    m.x = 1;
+    m.y = 2;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == false);
+    m.x = 2;
+    m.y = 1;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == false);
+    m.x = 2;
+    m.y = 2;
+    enterMove(g, m); 
+    assert(gameIsDraw(g) == true);
+    printf("gameIsDraw tests passed!\n"); 
+}
