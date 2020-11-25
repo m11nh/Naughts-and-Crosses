eca1b96ea623113b892dfd953e653ffab5aac673
Minh <minh.nguyen19997@hotmail.com>
2020-11-25 20:41:47 +1100

    removed unused file

--- game.c
+++
@@ -1,9 +0,0 @@
-#include stdio.h
-#include stdlib.h
-
-// runs instance of naughts and crosses game, deals with user input output?
-// e.g. if user enters in \q, the application closes
-int main(int argc, char * argv[]) {
-    
-
-}
